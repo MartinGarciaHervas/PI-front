2a868d13497e9e185e52e1b7ae14a42f7a1e5c13
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-31 13:44:07 -0300

    deploy 6

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import './index.css'
 import axios from 'axios';
 
 //Esto te permite no tener que repetir el url cada vez que queres hacer una peticion, solo pones la Ruta
-axios.defaults.baseURL = 'pi-back-production-9071.up.railway.app'
+axios.defaults.baseURL = 'https://pi-back-production-9071.up.railway.app'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
